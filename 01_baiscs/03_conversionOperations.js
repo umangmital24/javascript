028f729e604a75a08e1b0758a87414f1829ae931
Umang Mittal <117918427+umangmital24@users.noreply.github.com>
2024-09-02 09:03:32 +0000

    m

--- 01_baiscs/03_conversionOperations.js
+++ 01_baiscs/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = '33abc';
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => number
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn=1
 let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
 
 
 // 1 => true, 0=> false
@@ -24,8 +24,37 @@ console.log(booleanIsloggedIn);
 let someNumber  = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************************Operations********************* */
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " umang"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+let num1, num2, num3
+
+num1= num2 = num3 =2 +2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
 
 
