88f5643d30108ea404e79a93dbf1f13425563012
Umang Mittal <117918427+umangmital24@users.noreply.github.com>
2024-09-02 08:39:43 +0000

    sfas

---
+++ 01_baiscs/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail = "umang@google.com"
+var accountPassword = "12345"
+accountCity = "Kota"
+let accountState;
+//accountId = 2 //not allowed
+
+/*
+prefer not to use var
+*/
+accountEmail = "mittal@google.com"
+accountPassword = "21213131"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
---
+++ 01_baiscs/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version
+
+
+// alert(3+3) // we are using nodejs not brwoser
+
+console.log(3 +3);
+
+console.log("Umang");
+
+let name = "Umang"
+let Age = 20
+let isLoggedIn = false
+
+// number => 2 to poer 53
+//bigint
+// string =>""
+// null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof Age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
