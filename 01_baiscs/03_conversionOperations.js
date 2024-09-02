c4e2da8c12834c66c3709d5791b7313eb38153b6
Umang Mittal <117918427+umangmital24@users.noreply.github.com>
2024-09-02 08:52:47 +0000

    fsa

---
+++ 01_baiscs/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = '33abc';
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => number
+// "33abc" => NaN
+// true => 1; False => 0
+
+let isLoggedIn=1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+
+// 1 => true, 0=> false
+// " " => false
+//"umang" => true
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
